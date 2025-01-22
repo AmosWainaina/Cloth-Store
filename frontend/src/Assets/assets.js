01ccb54672a375855ed1d10f524eef2730ebae06
Amos Wainaina <toxicamos99@gmail.com>
2025-01-22 11:30:12 +0300

    Changes

---
+++ frontend/src/Assets/assets.js
@@ -0,0 +1,24 @@
+import img1 from './images/1.jpg';  
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
+import img7 from './images/7.jpg';
+import img8 from './images/8.jpg';
+import img9 from './images/9.jpg';
+import img10 from './images/10.jpg';
+import img11 from './images/11.jpg';
+import img12 from './images/12.jpg';
+import img13 from './images/13.jpg';
+import img14 from './images/14.jpg';
+import img15 from './images/15.jpg';
+
+export const products =[{
+    id:1,
+    name:"Product 1",
+    price:100,
+    image:[img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15],
+    description:"This is product 1"
+    },
+]
