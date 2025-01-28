d6485a9141fa8282ca9028cfc3796209435b8e2d
Amos Wainaina <toxicamos99@gmail.com>
2025-01-28 12:33:26 +0300

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import Shop from './pages/Shop.jsx';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -10,7 +13,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> { Home  }
+        <Route path="/shop" element={<Shop />} /> { Shop }
+        <Route path="/login" element={<Login />} /> { Login }
+        <Route path="/cart" element={<Cart />} /> { Cart }
       </Routes>
     </BrowserRouter>
   );
