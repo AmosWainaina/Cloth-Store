27f50f538b6a0bf961cba827cbe6f0679050670d
Amos Wainaina <toxicamos99@gmail.com>
2025-01-28 12:29:46 +0300

    Message

---
+++ frontend/src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+      <p>Explore our latest fashion collection.</p>
+    </div>
+  );
+};
+
+export default Shop;
+
