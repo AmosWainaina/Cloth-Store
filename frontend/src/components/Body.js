ac7ff633574709e3bb9df3af3a5313d2fb186e25
Amos Wainaina <136461041+AmosWainaina@users.noreply.github.com>
2025-02-19 12:33:18 +0300

    Feature/homepage (#9)
    * messagechanges
    
    * changes

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -48,6 +48,39 @@ const Body = () => {
         </div>
 
             
+
+            <div class="services">
+               <div class="container">
+                  <h1 class="sub-title">Why Choose Us</h1>
+                     <div class="services-list">
+                         <div>
+                            <i class="fa-solid fa-truck-fast"></i>
+                            <h2>Shipping</h2>
+                            <p>Free shipping on all orders over $50,and arrival within 3-5 business days</p>
+                            
+                         </div>
+    
+                        <div>
+                            <i class="fa-solid fa-receipt"></i>
+                            <h2>Availability</h2>
+                            <p>Available to you 24/7,365 days a year,With new products added daily</p>
+                            
+                        </div>
+    
+                        <div>
+                             <i class="fa-solid fa-medal"></i>
+                            <h2>Waranty</h2>
+                            <p>Extended warranty on all products,30 days money back guarantee</p>
+                            
+                        </div>
+                    </div>
+              </div>
+          </div>
+
+          
+
+
+            
         </div>
         
         
